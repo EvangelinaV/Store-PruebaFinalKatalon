c8635ed703c10350b35bdd15c1774f78d16c42cf
EvangelinaV <evangelina.vera@abstracta.com.uy>
2024-01-26 16:32:20 -0300

    Changes to registerUser test case

---
+++ Object Repository/Page_Register Account/div_Account      If you already have an acc_b8fddc.rs
@@ -0,0 +1,322 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Account      If you already have an acc_b8fddc</name>
+   <tag></tag>
+   <elementGuidId>69df0fd8-a2c3-40b8-8680-96eeaf5fd61a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='content']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#content</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>3f927581-2379-429d-bea1-2ff1c04b898b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>content</value>
+      <webElementGuid>4fe81c75-9695-4fc6-8ba4-091b5fb6e347</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>col-sm-9</value>
+      <webElementGuid>02e721a7-8248-4bd2-814f-29be648a3ab5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+      Account
+      If you already have an account with us, please login at the login page.
+      
+        
+          Your Personal Details
+          
+            Customer Group
+                                        
+                
+                  
+                  Default
+              
+                            
+          
+          
+            First Name
+            
+              
+               
+          
+          
+            Last Name
+            
+              
+               
+          
+          
+            E-Mail
+            
+              
+               
+          
+          
+            Telephone
+            
+              
+               
+          
+                  
+        
+          Your Password
+          
+            Password
+            
+              
+               
+          
+          
+            Password Confirm
+            
+              
+               
+          
+        
+        
+          Newsletter
+          
+            Subscribe
+                           
+                
+                Yes
+              
+                
+                No
+               
+          
+        
+        
+                
+          I have read and agree to the Privacy Policy
+                        
+                         
+            
+          
+        
+              
+      </value>
+      <webElementGuid>a897971a-5009-439b-8e29-b63d5aeccd30</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;content&quot;)</value>
+      <webElementGuid>2e9a26b4-8ad3-408b-a463-e3ae0b28ec02</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//div[@id='content']</value>
+      <webElementGuid>a255d600-ef2b-4e00-a0b7-ece195ba0bc1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='account-register']/div/div</value>
+      <webElementGuid>52e903f7-9493-44a5-95a0-9670840f92fa</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div</value>
+      <webElementGuid>b340c5fb-75f3-440b-bad6-04d01d619bbd</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[@id = 'content' and (text() = '
+      Account
+      If you already have an account with us, please login at the login page.
+      
+        
+          Your Personal Details
+          
+            Customer Group
+                                        
+                
+                  
+                  Default
+              
+                            
+          
+          
+            First Name
+            
+              
+               
+          
+          
+            Last Name
+            
+              
+               
+          
+          
+            E-Mail
+            
+              
+               
+          
+          
+            Telephone
+            
+              
+               
+          
+                  
+        
+          Your Password
+          
+            Password
+            
+              
+               
+          
+          
+            Password Confirm
+            
+              
+               
+          
+        
+        
+          Newsletter
+          
+            Subscribe
+                           
+                
+                Yes
+              
+                
+                No
+               
+          
+        
+        
+                
+          I have read and agree to the Privacy Policy
+                        
+                         
+            
+          
+        
+              
+      ' or . = '
+      Account
+      If you already have an account with us, please login at the login page.
+      
+        
+          Your Personal Details
+          
+            Customer Group
+                                        
+                
+                  
+                  Default
+              
+                            
+          
+          
+            First Name
+            
+              
+               
+          
+          
+            Last Name
+            
+              
+               
+          
+          
+            E-Mail
+            
+              
+               
+          
+          
+            Telephone
+            
+              
+               
+          
+                  
+        
+          Your Password
+          
+            Password
+            
+              
+               
+          
+          
+            Password Confirm
+            
+              
+               
+          
+        
+        
+          Newsletter
+          
+            Subscribe
+                           
+                
+                Yes
+              
+                
+                No
+               
+          
+        
+        
+                
+          I have read and agree to the Privacy Policy
+                        
+                         
+            
+          
+        
+              
+      ')]</value>
+      <webElementGuid>43526a04-626c-4662-8465-c7a2f1831728</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
